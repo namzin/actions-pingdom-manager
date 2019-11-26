5eb458a7992b82fb1e036301ce9d0c512145b8c6
Damian Sloane <dddaaammmooo@hotmail.com>
2019-11-26 15:32:34 +1100

    Echo filename for debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,4 +20,5 @@ pip install ./requirements.txt
 
 # Execute script
 CONFIG_FILENAME=$(echo "${GITHUB_WORKSPACE}/${FILENAME}" | tr -s /)
+echo "Loading Configuration File: $C{CONFIG_FILENAME}"
 python ./pingdom.py "${CONFIG_FILENAME}" "${PINGDOM_API_KEY}"
